ff937ce635a1782461e37fc75529283dc6950719
Your Name <you@example.com>
2026-03-26 20:07:17 +0000

    new try

--- list.c
+++ list.c
@@ -96,8 +96,8 @@ void * popCurrent(List * list) {
     
     void* dato_eliminado = actual -> data;
     if (list -> current == list -> head){
-    list -> head = list -> current -> next;
-    list -> head -> prev = NULL;
+        list -> head = list -> current -> next;
+        list -> head -> prev = NULL;
     } 
     prevEliminado -> next = list -> current -> next;
     nextEliminado -> prev = list -> current -> prev;
